9ea59d95ceac89ada3a2eb80e9de5420f5db20d7
Shubham <sbarwad20595@gmail.com>
2023-02-14 15:59:45 +0530

    "Added Reviews and Subscribe section"

--- js/script.js
+++ js/script.js
@@ -106,4 +106,30 @@ var swiper = new Swiper(".vehicles-slider", {
       },
     },
   });
+  var swiper = new Swiper(".reviews-slider", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1300: {
+        slidesPerView: 3,
+      },
+    },
+  });
 // Swiper effects ends
